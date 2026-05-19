838548259f04dc5350d5dd7f011b31a8aa826045
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2026-05-18 22:00:46 -0300

    Creacion del proyecto Human-Ware

---
+++ Human-Ware/src/main/java/vista/human/ware/HumanWare.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vista.human.ware;
+
+/**
+ *
+ * @author FRANCO
+ */
+public class HumanWare {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
