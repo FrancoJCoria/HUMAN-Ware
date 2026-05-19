8dbe6cf3b999c6e06a46a851d6d1326b53bddb43
Emi-legui <emilegui76@gmail.com>
2026-05-19 01:18:23 -0300

    agregando atributos, constructor, get y set a clase empresa

--- Human-Ware/src/main/java/vista/human/ware/Empresa.java
+++ Human-Ware/src/main/java/vista/human/ware/Empresa.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package vista.human.ware;
 
-/**
- *
- * @author Noxie-PC
- */
 public class Empresa {
+    //atributos
+    private int idEmpresa;
+    private String nombre;
+    private String correo;
+    private String telefono;
+
+    //constructor
+    public Empresa (int idEmpresa, String nombre, String correo, String telefono){
+        this.idEmpresa = idEmpresa;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono; 
+    }
+//get y set
+    public int getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(int idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     
 }
