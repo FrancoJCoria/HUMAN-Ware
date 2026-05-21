3f0aed1aa1f0986981cabe7370e6d6c1483a9e19
Emi-legui <emilegui76@gmail.com>
2026-05-21 09:20:31 -0300

    Clase Hablididad: agrege Atributos, constructor, get/set y metodos

--- Human-Ware/src/main/java/vista/human/ware/Habilidad.java
+++ Human-Ware/src/main/java/vista/human/ware/Habilidad.java
@@ -9,5 +9,39 @@ package vista.human.ware;
  * @author FRANCO
  */
 class Habilidad {
+    //Atributos
+    private int idHabilidad;
+    private String nombre;
+    
+    //Constructor
+    public Habilidad(int idHabilidad, String nombre) {
+        this.idHabilidad = idHabilidad;
+        this.nombre = nombre;
+    }
+     //Constructor Vacio
+    public Habilidad() {
+    }
+    
+     //Get y Set
+    public int getIdHabilidad() {
+        return idHabilidad;
+    }
+
+    public void setIdHabilidad(int idHabilidad) {
+        this.idHabilidad = idHabilidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //Metodos
+    public void mostrarHabilidad(){
+        System.out.println("ID: "+ this.idHabilidad + "  " + this.nombre);
+    }
     
 }
